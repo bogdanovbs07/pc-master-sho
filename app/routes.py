7701bf8e012d698fa23e34bc304d52f200ccb068
bogdanovbs07 <bogdanovbs07@yandex.ru>
2026-04-27 15:53:39 +0300

    Добавлен маршрут для страницы контактов

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
